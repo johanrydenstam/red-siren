a1c63af818f4fbac4b83f7624d8bff9301f6ef10
a.nvlkv <a.nvlkv@protonmail.ch>
2023-11-03 15:09:46 +0700

    init dioxus

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+#![allow(non_snake_case)]
+#![allow(unused)]
+use dioxus_fullstack::prelude::*;
+
+use dioxus::prelude::*;
+use log::LevelFilter;
+
+fn main() {
+    // Init debug
+    dioxus_logger::init(LevelFilter::Info).expect("failed to init logger");
+
+    let config = LaunchBuilder::new(app);
+    #[cfg(feature = "ssr")]
+    let config = config.incremental(
+        IncrementalRendererConfig::default().invalidate_after(std::time::Duration::from_secs(120)),
+    );
+
+    config.launch();
+}
+
+fn app(cx: Scope) -> Element {
+    cx.render(rsx! (
+        div {
+            style: "text-align: center;",
+            h1 { "🌗 Dioxus 🚀" }
+            h3 { "Frontend that scales." }
+            p { "Dioxus is a portable, performant, and ergonomic framework for building cross-platform user interfaces in Rust." }
+        }
+    ))
+}
+
+#[server(PostServerData)]
+async fn post_server_data(data: String) -> Result<(), ServerFnError> {
+    println!("Server received: {}", data);
+
+    Ok(())
+}
+
+#[server(GetServerData)]
+async fn get_server_data() -> Result<String, ServerFnError> {
+    Ok("Hello from the server!".to_string())
+}
